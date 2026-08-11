ef94b5d030bc24355738c36f2c7b0d05b3a8cecf
Alex <alexong35@gmail.com>
2026-08-11 20:40:19 +0800

    new files

---
+++ Car.java
@@ -0,0 +1,21 @@
+
+// Blue Print 
+public class Car {
+    // Attributes
+    String brand;
+    String colour;
+    int speed;
+
+    // Methods
+    public void drive()
+    {
+        System.out.println(brand + " is driving at "+ speed + " Km/h");
+    }
+
+    void stop()
+    {
+        System.out.println(brand + " has stopped ");
+    }
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    
+    public static void main(String[] args) {
+
+        // Create an Object from Class Car
+        Car myCar = new Car();
+
+        //Assign values to the object's attributes
+        myCar.brand="Toyota";
+        myCar.colour="Red";
+        myCar.speed=80;
+
+        // Call the object's methods
+        myCar.drive();
+        myCar.stop();
+
+
+        
+    }
+}
